e34f910c58c5214b02307661cf8027b9b8e464b3
xavcz <xavier.cazalot@gmail.com>
2017-10-11 14:23:10 +0200

    Exercise 5 #apollo-link
    [UI]
    
    ### Goal
    Catch errors send by the GraphQL API
    
    ### Instructions
    1. Create an `errorLink` using `onError` from `apollo-link-error`: the link should `console.log` the errors
    2. Compose it with the other links with the method of your choice
    
    ---
    ### Tips
    - Edit `web/src/client.js`
    - `onError` is a named export from `apollo-link-error`
    - `onError` is a *non-terminating* link. It should be composed before the `.split(...)`!
    
    ### Docs
    - [Error Handling with Apollo Link](https://www.youtube.com/watch?v=bv74TcKb1jw)
    - [Apollo Link Error](https://github.com/apollographql/apollo-link/tree/master/packages/apollo-link-error)

--- web/src/client.js
+++ web/src/client.js
@@ -3,10 +3,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error';
 import { HttpLink } from 'apollo-link-http';
 import WsLink from 'apollo-link-ws';
 import Cache from 'apollo-cache-inmemory';
 import App from './components/App';
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  // could also be sent to an error logger
+  console.error('Apollo Errors caught!', { graphqlErrors, networkError });
+});
 
 const authLink = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem('AUTH_TOKEN');
@@ -29,14 +35,17 @@ const hasSubscriptionOperation = operation =>
 // http://localhost:8080 is put behind a proxy by webpack (cors)
 const uri = '/graphql';
 
-const link = authLink.split(
-  hasSubscriptionOperation,
-  new WsLink({
-    uri: 'ws://localhost:8081/graphql',
-    options: { reconnect: true },
-  }),
-  new HttpLink({ uri })
-);
+const link = ApolloLink.from([
+  errorLink,
+  authLink.split(
+    hasSubscriptionOperation,
+    new WsLink({
+      uri: 'ws://localhost:8081/graphql',
+      options: { reconnect: true },
+    }),
+    new HttpLink({ uri })
+  ),
+]);
 
 const cache = new Cache();
 
