c5001db016fef2a0a4fd73b7e75982546fe55183
xavcz <xavier.cazalot@gmail.com>
2017-10-19 14:50:46 +0200

    Exercise 1 #apollo-link
    [UI]
    
    ### Goal
    Enable login by adding auth headers to network requests
    
    ### Instructions
    1. Create a new `ApolloLink` instance
      1. Get the auth token from the local storage (`localStorage.getItem('AUTH_TOKEN')`)
      2. Use `operation.setContext` to send the `authorization` header
      3. Forward the operation to the next link
    2. Concatenate the `HttpLink` with the new link
    3. Log into the app with "John Doe".
    4. Log out and create a new account
    
    ---
    ### Tips
    - Edit `web/src/client.js`
    
    ### Docs
    - [Additive composition](https://github.com/apollographql/apollo-link/blob/master/docs/source/composition.md#additive-composition)

--- ui/src/client.js
+++ ui/src/client.js
@@ -2,14 +2,27 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './components/App';
 
+const authLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem('AUTH_TOKEN');
+
+  operation.setContext(() => ({
+    headers: {
+      authorization: token ? `JWT ${token}` : null,
+    },
+  }));
+
+  return forward(operation);
+});
+
 // http://localhost:8080 is put behind a proxy by webpack (cors)
 const uri = '/graphql';
 
-const link = new HttpLink({ uri });
+const link = authLink.concat(new HttpLink({ uri }));
 
 const cache = new InMemoryCache();
 
