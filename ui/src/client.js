09f759c2eddf349c65b60986581622b983e5d80a
xavcz <xavier.cazalot@gmail.com>
2018-01-16 12:14:17 +0100

    Enable login by adding auth headers to network requests #client-architecture
    [UI]
    
    ### Instructions
    1. Create a new `ApolloLink` instance
      1. Get the auth token from the local storage (`localStorage.getItem('AUTH_TOKEN')`)
      2. Use `operation.setContext` to send the `authorization` header
      3. Forward the operation to the next link
    2. Concatenate the `HttpLink` with the new link
    3. Log into the app with "John Doe".
    4. Log out and create a new account
    
    ---
    ### Tips
    - Edit `web/src/client.js`
    
    ### Docs
    - [Additive composition](https://www.apollographql.com/docs/link/composition.html#additive)

--- ui/src/client.js
+++ ui/src/client.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './App/App';
 
-// create an HttpLink to connect to the GraphQL API: the uri is http://localhost:8080/graphql
-const link = new HttpLink({ uri: 'http://localhost:8080/graphql' });
+const authLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem('AUTH_TOKEN');
+
+  operation.setContext(() => ({
+    headers: {
+      authorization: token ? `JWT ${token}` : null,
+    },
+  }));
+
+  return forward(operation);
+});
+
+const link = authLink.concat(new HttpLink({ uri: 'http://localhost:8080/graphql' }));
 
 // create an InMemoryCache to store GraphQL operations results
 const cache = new InMemoryCache();
