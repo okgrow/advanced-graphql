9aaf1beaf228f8b90a52f6c8008986b9b0edd7b8
xavcz <xavier.cazalot@gmail.com>
2018-03-09 16:02:22 +0100

    Intercept errors with `apollo-link-error` #error-handling
    [UI]
    
    ### Instructions
    1. Create an `errorLink` thanks to `onError` from `apollo-link-error`
    2. Intercept `graphqlErrors` and `networkerror` with `console.log`
    2. Add the link to the chain of links already present
    
    ---
    ### Tips
    Edit `ui/src/client.js`
    Re-use the code from the previous slides
    
    ### Notes
    [`apollo-link-error` docs](https://www.apollographql.com/docs/link/links/error.html)

--- ui/src/client.js
+++ ui/src/client.js
@@ -5,6 +5,7 @@ import { ApolloClient } from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { WebSocketLink } from 'apollo-link-ws';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './components/App';
 
@@ -26,13 +27,27 @@ const hasSubscriptionOperation = operation =>
     false
   );
 
-const link = authLink.split(
-  hasSubscriptionOperation,
-  new WebSocketLink({
-    uri: 'ws://localhost:8081/graphql',
-    options: { reconnect: true },
-  }),
-  new HttpLink({ uri: 'http://localhost:8080/graphql' })
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+    );
+  }
+
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
+
+const link = errorLink.concat(
+  authLink.split(
+    hasSubscriptionOperation,
+    new WebSocketLink({
+      uri: 'ws://localhost:8081/graphql',
+      options: { reconnect: true },
+    }),
+    new HttpLink({ uri: 'http://localhost:8080/graphql' })
+  )
 );
 
 // create an InMemoryCache to store GraphQL operations results
