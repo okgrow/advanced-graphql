255c144ae061dc42e975f3e2d91318656c9cb9f8
Robert Dickert <robert.dickert@gmail.com>
2018-10-03 18:10:39 -0600

    Update all connected clients when a place is created #graphql-subscriptions
    [UI]
    
    ### Instructions
    1. Create a .gql file with the document for the subscription: `subscription placeCreated { placeCreated { ... } }`
    2. Consume this document in `ui/src/App/App.js` using `subscribeToMore` inside the `Query` component.
    3. Update the cache with the result in `updateQuery` by appending the new result to the end of the array. (it will appear in reverse-chronological order because we reverse the list in css)
    4. Open a 2nd window to the app and confirm that when you create a new place in one window, it appears in the other as well.
    
    ---
    ### Tips
    - The data to return from `updateQuery` callback are the previous places + the new one: it's as similar to the example shown in the Apollo docs (see link below)
    - You may encounter a problem where a new item gets added twice. See the code in `ui/src/App/BucketList/AddPlace/AddPlace.js` to see where else a new place might be added and an example solution. Discussion question: is there a solution to having possible conflicting updates?
    
    ### Docs
    - [`subscribeToMore` documentation and example](https://www.apollographql.com/docs/react/advanced/subscriptions.html#subscribe-to-more)

--- ui/src/App/App.js
+++ ui/src/App/App.js
@@ -4,6 +4,7 @@ import { Query, Subscription } from 'react-apollo';
 import Routes from './Routes';
 import getPlacesQuery from '../graphql/getPlaces.query.gql';
 import updateplaceSubcription from './updatePlace.subscription.gql';
+import createPlaceSubcription from './createPlace.subscription.gql';
 
 import Loading from './Loading';
 import Error from './Error';
@@ -14,9 +15,31 @@ const App = () => (
   <div>
     <Query query={getPlacesQuery}>
       {graphqlResponse => {
-        const { data, loading, error } = graphqlResponse;
+        const { data, loading, error, subscribeToMore } = graphqlResponse;
         if (loading) return <Loading />;
         if (error) return <Error />;
+        subscribeToMore({
+          document: createPlaceSubcription,
+          updateQuery: (prev, { subscriptionData }) => {
+            const { placeCreated } = subscriptionData.data;
+            if (!placeCreated) {
+              return prev;
+            }
+
+            // Check to see if the addPlace mutation already added our place.
+            // Note: Mutation & sub can happen in either order, so you have to check in
+            // the mutation update function as well!
+            if (prev.places.find(place => place.id === placeCreated.id)) {
+              return prev;
+            }
+
+            // If not, append the new place to the collection.
+            return {
+              ...prev,
+              places: [...prev.places, placeCreated],
+            };
+          },
+        });
         return <Routes places={data.places} />;
       }}
     </Query>
