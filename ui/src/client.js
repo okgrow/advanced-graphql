5b0ac94db9b5a7abf5c2e90990f953885bf94f12
Christopher Cuilla <ccuilla@gmail.com>
2019-02-06 07:32:05 -0700

    Enable a web socket link to see changes in checkbox (visited) status across browser windows #GraphQL-Subscriptions
    [UI]
    
    ### Instructions
    1. Create a directional composition from the `authLink`:
      - the direction is established with `hasSubscriptionOperation` (see previous slides)
      - subscriptions should be sent to a `WsLink` from `apollo-link-ws`
      - queries/mutations should be sent to the `HttpLink`
    2. Confirm that http requests are still working
    3. Copy your subscription to a new file `ui/src/App/updatePlace.subscription.gql`
    4. Use the <Subscribe> component to access the subscription. Do this in `ui/src/App/App.js`. You don't need to render anything inside the component.
    5. Confirm everything is working by opening two windows on the BucketList - changing the visited checkbox in one should change it in the other. Discussion question: How is the checkbox updated without writing `update` code?
    
    ---
    ### Tips
    - Create a `WsLink` with params as shown in the slides: the url to reach is `ws://localhost:8081`
    - Change `authLink` method from `.concat` to `.split`
    - In the `split(...)`, the first link should be the `WsLink`, the second link the `HttpLink`
    
    ### Docs
    - [Directional composition](https://www.apollographql.com/docs/link/composition.html#directional)

--- ui/src/client.js
+++ ui/src/client.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './App/App';
 
@@ -19,7 +20,18 @@ const authLink = new ApolloLink((operation, forward) => {
   return forward(operation);
 });
 
-const link = authLink.concat(
+const hasSubscriptionOperation = operation =>
+  operation.query.definitions.reduce(
+    (result, definition) => result || definition.operation === 'subscription',
+    false
+  );
+
+const link = authLink.split(
+  hasSubscriptionOperation,
+  new WebSocketLink({
+    uri: 'ws://localhost:4001/graphql',
+    options: { reconnect: true },
+  }),
   new HttpLink({ uri: 'http://localhost:4000/graphql' })
 );
 
