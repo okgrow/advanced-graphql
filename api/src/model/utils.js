tamouse/OkGrow-GraphQLSummit-2017-advanced-graphql-workshop

⊂(◕‿◕)つ ADVANCED GRAPHQL

api/src/model/utils.js
@@ -1,0 +1,68 @@
+import { ObjectId } from 'mongodb';
+
+export const fromMongo = item => ({
+  ...item,
+  id: item._id.toString(),
+});
+
+export const findByIds = async (collection, ids) => {
+  try {
+    const _ids = ids.map(id => new ObjectId(id));
+
+    // uncomment to show how it works
+    // console.log('// running findByIds', collection.namespace, ids);
+
+    // get all unique results in one query
+    const docs = await collection.find({ _id: { $in: _ids } }).toArray();
+
+    // make a normalized object to map ids to db results
+    const idsMap = docs
+      .map(fromMongo)
+      .reduce((map, doc) => ({ ...map, [doc.id]: doc }), {});
+
+    // return results for each id in the order provided
+    return ids.map(id => idsMap[id]);
+  } catch (e) {
+    console.error(e);
+    throw new Error('Something bad happened with the Mongo loader');
+  }
+};
+
+export const seedDb = async ({
+  db,
+  pubsub,
+  Place: PlaceModel,
+  User: UserModel,
+  reset,
+}) => {
+  const Place = new PlaceModel({ db, pubsub });
+  const User = new UserModel({ db, pubsub });
+
+  if (reset) {
+    await Place.collection.remove({});
+    await User.collection.remove({});
+  }
+
+  const places = await Place.all({});
+
+  if (places.length) {
+    console.log('Database has entries.');
+    return;
+  }
+
+  const userSeed = { username: 'John Doe' };
+
+  const placeSeeds = [
+    { name: 'San Francisco', visited: true },
+    { name: 'Toronto', visited: true },
+    { name: 'Grand Canyon', visited: false },
+  ];
+
+  const _id = await User.insert(userSeed);
+
+  for (const seed of placeSeeds) {
+    await Place.insert({ ...seed, userId: _id.toString() });
+  }
+
+  console.log('Database seeded!');
+};

ui/src/components/shared/Header.js
@@ -1,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = props => {
+  const title = props.title || 'Default title';
+
+  return (
+    <Strip>
+      <Title>{title.toUpperCase()}</Title>
+    </Strip>
+  );
+};
+
+const Strip = styled.div`
+  background-color: var(--darkBlue);
+  padding-top: 3rem;
+  padding-bottom: 1rem;
+  margin-bottom: 3rem;
+  justify-content: center;
+  align-self: stretch;
+`;
+
+const Title = styled.div`
+  font-size: 3.5rem;
+  font-weight: bold;
+  letter-spacing: 0.5rem;
+  color: var(--lightBlue);
+  text-align: center;
+`;
+
+export default Header;

okgrow/advanced-graphql

api/src/model/utils.js
@@ -1,0 +1,68 @@
+import { ObjectId } from 'mongodb';
+
+export const fromMongo = item => ({
+  ...item,
+  id: item._id.toString(),
+});
+
+export const findByIds = async (collection, ids) => {
+  try {
+    const _ids = ids.map(id => new ObjectId(id));
+
+    // uncomment to show how it works
+    console.log('// running findByIds', collection.namespace, ids);
+
+    // get all unique results in one query
+    const docs = await collection.find({ _id: { $in: _ids } }).toArray();
+
+    // make a normalized object to map ids to db results
+    const idsMap = docs
+      .map(fromMongo)
+      .reduce((map, doc) => ({ ...map, [doc.id]: doc }), {});
+
+    // return results for each id in the order provided
+    return ids.map(id => idsMap[id]);
+  } catch (e) {
+    console.error(e);
+    throw new Error('Something bad happened with the Mongo loader');
+  }
+};
+
+export const seedDb = async ({
+  db,
+  pubsub,
+  Place: PlaceModel,
+  User: UserModel,
+  reset,
+}) => {
+  const Place = new PlaceModel({ db, pubsub });
+  const User = new UserModel({ db, pubsub });
+
+  if (reset) {
+    await Place.collection.remove({});
+    await User.collection.remove({});
+  }
+
+  const places = await Place.all({});
+
+  if (places.length) {
+    console.log('Database has entries.');
+    return;
+  }
+
+  const userSeed = { username: 'John Doe' };
+
+  const placeSeeds = [
+    { address: 'San Francisco', visited: true },
+    { address: 'Toronto', visited: true },
+    { address: 'Grand Canyon', visited: false },
+  ];
+
+  const _id = await User.insert(userSeed);
+
+  for (const seed of placeSeeds) {
+    await Place.insert({ ...seed, userId: _id.toString() });
+  }
+
+  console.log('Database seeded!');
+};

ui/src/components/Header.js
@@ -1,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = props => {
+  const title = props.title || 'Default title';
+
+  return (
+    <Strip>
+      <Title>{title.toUpperCase()}</Title>
+    </Strip>
+  );
+};
+
+const Strip = styled.div`
+  background-color: var(--peterRiver);
+  padding-top: 3rem;
+  padding-bottom: 1rem;
+  margin-bottom: 3rem;
+  justify-content: center;
+  align-self: stretch;
+`;
+
+const Title = styled.div`
+  font-size: 3.5rem;
+  font-weight: bold;
+  color: var(--clouds);
+  text-align: center;
+  font-family: Gaegu, sans-serif;
+`;
+
+export default Header;